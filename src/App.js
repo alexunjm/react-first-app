f2730d0a7b0c86bb6bc8d30b56ad572f696e692e
alexun <alexunjm@gmail.com>
2018-04-13 11:58:00 -0500

    ejemplo de método que permite cambiar el estado al dar click al boton

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import "./App.css";
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    title: "Bienvenido a React",
+    pText: "Esto realmente funciona!!"
+  };
 
-	state = {
-		title: 'Bienvenido a React'
-	};
+  switchNameHandler = () => {
+    //console.log('se ha dado click');
+    //NO HACER ESTO: this.state.title = 'nuevo título';
+    this.setState({
+      title: "Ejemplo de evento de click para cambiar el título de ésta página"
+    });
+  };
 
-	switchNameHandler() {
-		console.log('se ha dado click');
-	}
-
-  	render() {
-             /* return (
+  render() {
+    /* return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -25,18 +29,22 @@ class App extends Component {
         </p>
       </div>
 	); */
-	
-		return <div className="App">
+
+    return (
+      <div className="App">
         <h1>{this.state.title}</h1>
-        <p>Esto realmente funciona!!</p>
-		<button onClick={this.switchNameHandler} >cambiar nombre</button>
+        <p>{this.state.pText}</p>
+        <button onClick={this.switchNameHandler}>cambiar nombre</button>
         <Person name="Alex" age="29" />
-        <Person name="Andrea" age="30" >Mis hobbies son: cocinar, ver series</Person>
+        <Person name="Andrea" age="30">
+          Mis hobbies son: cocinar, ver series
+        </Person>
         <Person name="Marlon" age="29" />
-      </div>;
-	
-		/* return React.createElement("div", { className: "App" }, React.createElement("h1", null, "Ejemplo de componente con la clase React")); */
-	}
+      </div>
+    );
+
+    /* return React.createElement("div", { className: "App" }, React.createElement("h1", null, "Ejemplo de componente con la clase React")); */
+  }
 }
 
 export default App;
