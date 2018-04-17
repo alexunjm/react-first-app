8403a5053ea57fa52c181e7dbe7e9e31b5538474
alexun <alexunjm@gmail.com>
2018-04-16 19:19:51 -0500

    clases dinámicas

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 const person = (props) => {
+
+	const classes = [];
+	if(props.name.length <= 10) {
+		classes.push('red');
+	}
+	if(props.name.length <= 5) {
+		classes.push("bold");
+	}
+	console.log(classes);
+
 	return (
 		<div>
-			<p>Soy {props.name} y tengo {props.age} años!</p>
+			<p className={classes.join(' ')}>Soy {props.name} y tengo {props.age} años!</p>
 			{props.children != null && <p onClick={props.click}>{props.children}</p>}
 			<input type="text" onChange={props.changed} value={props.name} />
 		</div>
